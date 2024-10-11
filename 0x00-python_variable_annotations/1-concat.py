d307b3bdad094500ebd46568fc1c6103f472ffdb
Ibukun16 <ib.agunbiade18@gmail.com>
2024-10-11 04:47:25 +0100

    update task 1 to return concatenated string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""A type-annotated function that takes
+2 different strings arguments and
+returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str): -> str:
+    """Concatenate two strings"""
+    return str1 + str2
