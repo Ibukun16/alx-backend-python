d3e54bca7172511373c36881369069190e650a0d
Ibukun16 <ib.agunbiade18@gmail.com>
2024-10-10 21:17:31 -0700

    Update task 3 and 4

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function that takes a float n as
+argument and returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """Convert a floating-point number to string"""
+    return str(n)
